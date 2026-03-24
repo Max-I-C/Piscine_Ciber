d2e88247fda6fafec0b86d7a7cf66ef0f82e921a
Max-I-C <maxence.isaac.c@gmail.com>
2026-03-24 15:16:53 +0100

    adding the first ex

---
+++ spider/spider.py
@@ -0,0 +1,74 @@
+
+import requests
+import os
+import argparse
+from bs4 import BeautifulSoup
+from genericpath import exists
+from urllib.parse import urljoin
+
+
+visited = set()
+
+def args_manager():
+    parser = argparse.ArgumentParser(description="Learn2Slither")
+    parser.add_argument(
+        "-p", "--path",
+        type=str,
+        help="To indicate a path for saving the images"
+    )
+    parser.add_argument(
+        "-r", "--recursive",
+        help="Turn to program in recurssive mode"
+    )
+    parser.add_argument(
+        "-l", "--layer",
+        type=int,
+        default=5,
+        help="Specify the layer quantity you want to explore"
+    )
+    args = parser.parse_args()
+    return(args)
+
+
+def curl(path, url, recurssif, layer):
+    if(layer <= 0 or url in visited):
+        return
+    visited.add(url) # Peut être ajouter sa a la fin, jsp sah #
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for image in soup.find_all('img'):
+        if (image.get('src')) != None:
+            print(image.get('src'))
+            img_url = urljoin(url, image.get('src')) 
+            if(img_url.endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))):
+                if(path is not None):
+                    name = path + "/" + img_url.split('/')[-1]
+                else:
+                    name = '.data/' + img_url.split('/')[-1]
+                img = requests.get(img_url)
+                file = open(name ,'wb')
+                file.write(img.content)
+                file.close()
+            # Ajouter la logique pour les liens #
+            if(recurssif):
+                for link in soup.find_all('a'):
+                    if(link.get('href') != None):
+                        link_url = urljoin(url, link.get('href'))
+                        curl(path, link_url, True, layer - 1)
+                
+
+def main():
+
+    args = args_manager()
+    if args.path is not None and not exists(args.path):
+        os.mkdir(args.path)
+    elif (not exists(".data") and args.path is None):
+        os.mkdir('.data')
+    curl(args.path, 'https://www.permisdeconduire-online.be/', True, 2)
+
+
+    
+            
+
+if(__name__ == "__main__"):
+    main()
