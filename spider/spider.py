423da848f4c36144bfac136808912a2d92fe7aa5
Max <maxence.isaac.c@gmail.com>
2026-03-24 18:00:23 +0100

    commit

--- spider/spider.py
+++ spider/spider.py
@@ -5,12 +5,13 @@ import argparse
 from bs4 import BeautifulSoup
 from genericpath import exists
 from urllib.parse import urljoin
+from requests import RequestException
 
 
 visited = set()
 
 def args_manager():
-    parser = argparse.ArgumentParser(description="Learn2Slither")
+    parser = argparse.ArgumentParser(description="Spider")
     parser.add_argument(
         "-p", "--path",
         type=str,
@@ -18,6 +19,8 @@ def args_manager():
     )
     parser.add_argument(
         "-r", "--recursive",
+        type=bool,
+        default=False,
         help="Turn to program in recurssive mode"
     )
     parser.add_argument(
@@ -33,42 +36,45 @@ def args_manager():
 def curl(path, url, recurssif, layer):
     if(layer <= 0 or url in visited):
         return
-    visited.add(url) # Peut être ajouter sa a la fin, jsp sah #
-    response = requests.get(url)
+    visited.add(url)
+    try:
+        response = requests.get(url)
+    except RequestException as e:
+        print("Error with ", {e})
+        return
     soup = BeautifulSoup(response.text, 'html.parser')
     for image in soup.find_all('img'):
         if (image.get('src')) != None:
-            print(image.get('src'))
             img_url = urljoin(url, image.get('src')) 
             if(img_url.endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))):
                 if(path is not None):
                     name = path + "/" + img_url.split('/')[-1]
                 else:
                     name = '.data/' + img_url.split('/')[-1]
-                img = requests.get(img_url)
-                file = open(name ,'wb')
-                file.write(img.content)
-                file.close()
-            # Ajouter la logique pour les liens #
-            if(recurssif):
-                for link in soup.find_all('a'):
-                    if(link.get('href') != None):
-                        link_url = urljoin(url, link.get('href'))
-                        curl(path, link_url, True, layer - 1)
-                
+                try:
+                    img = requests.get(img_url)
+                    if(img.status_code != 404):
+                        print(img_url)
+                        file = open(name ,'wb')
+                        file.write(img.content)
+                        file.close()
+                except RequestException as e:
+                    print("Error with ", {e})
+    # Ajouter la logique pour les liens #
+    if(recurssif):
+        for link in soup.find_all('a'):
+            if(link.get('href') != None):
+                link_url = urljoin(url, link.get('href'))
+                curl(path, link_url, True, layer - 1)
 
 def main():
-
     args = args_manager()
+    print(args.recursive)
     if args.path is not None and not exists(args.path):
         os.mkdir(args.path)
     elif (not exists(".data") and args.path is None):
         os.mkdir('.data')
-    curl(args.path, 'https://www.permisdeconduire-online.be/', True, 2)
-
-
-    
-            
+    curl(args.path, 'https://www.42madrid.com/', args.recursive, args.layer)
 
 if(__name__ == "__main__"):
     main()
