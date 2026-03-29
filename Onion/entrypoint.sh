e1168dd62fcfa1a44547ff6593bb4b572aae4d25
Max-I-C <maxence.isaac.c@gmail.com>
2026-03-29 15:53:48 +0200

    adding ssh service

--- Onion/entrypoint.sh
+++ Onion/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
+# Ensure runtime directory exists for sshd
+mkdir -p /var/run/sshd
+
 # Fix permissions for mounted volume
 chown -R tor:tor /var/lib/tor/hidden_service
 chmod 700 /var/lib/tor/hidden_service
 
 # Start Tor as tor user
 su tor -s /bin/sh -c "tor -f /etc/tor/torrc --RunAsDaemon 0" &
+
+# Start OpenSSH daemon in background
+/usr/sbin/sshd
 
 # Start nginx as root
 exec nginx -g 'daemon off;'
--- Onion/start.sh
+++ Onion/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build -t torwebnew . && \
-ID=$(docker run -d -v tor-hidden-service:/var/lib/tor/hidden_service torwebnew) && \
+ID=$(docker run -d -p 4242:4242 -v tor-hidden-service:/var/lib/tor/hidden_service torwebnew) && \
 echo "Container $ID launched. Generating the address..." && \
 until docker exec $ID test -f /var/lib/tor/hidden_service/hostname; do sleep 1; done && \
 echo -e "\n.Onion address :" && \
