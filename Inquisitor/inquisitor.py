ba82c9b51abf1ae43402d54d54805550cab04dbd
Max <maxence.isaac.c@gmail.com>
2026-04-01 17:12:08 +0200

    working on the arguument parsing

--- Inquisitor/inquisitor.py
+++ Inquisitor/inquisitor.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 import argparse
 import sys
+import ipaddress
 
 class Inquisitor():
     def __init__(self, argument):
@@ -9,8 +11,17 @@ class Inquisitor():
         self.mac_addr_serv = argument[4]
         print(f"Constructor, inquisition set : {self.mac_addr_host} + {self.ip_addr_host} + {self.mac_addr_serv} + {self.ip_addr_serv}")
 
-    def poisoning():
+    def verify_addr(self):
+        try:
+            ipaddress.IPv4Address(self.ip_addr_host)
+            ipaddress.IPv4Address(self.ip_addr_serv)
+        except Exception as e:
+            print(f"[ERROR], {e}")
+
+
+    def poisoning(self):
         print("All the logic about the change of IP in the networks")
+        self.verify_addr()
 
 def main():
     if(len(sys.argv) < 5):
